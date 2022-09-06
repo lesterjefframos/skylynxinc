58c05f1eebc5a04eec303812431fec09bb48dc33
lesterjefframos <35057921+lesterjefframos@users.noreply.github.com>
2022-09-06 23:22:26 +0800

    fixed body scroll on modal open

--- js/modal.js
+++ js/modal.js
@@ -1,15 +1,25 @@
+const body = document.body;
 const btns = document.querySelectorAll("[data-target]");
 const closeBtns = document.querySelectorAll(".modal-close");
 
 btns.forEach(btn => {
     btn.addEventListener('click', () => {
         document.querySelector(btn.dataset.target).classList.add("modal-show");
+        document.body.classList.add("no-scroll");
     });
 });
 
 closeBtns.forEach(btn => {
     btn.addEventListener('click', () => {
-        document.querySelector(btn.dataset.target).classList.remove("modal-show")
+        document.querySelector(btn.dataset.target).classList.remove("modal-show");
+        document.body.classList.remove("no-scroll");
     });
 });
+
+// body.addEventListener("click", function bodyListner(e) {
+//     if (e.target.class === ("show")) {
+//         body.classList.add("no-scroll");
+//         toggleViewportScrolling(true);
+//     }
+// })
 
