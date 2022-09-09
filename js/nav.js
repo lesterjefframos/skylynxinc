0021f3198c406a04761499497ea69f5f9f546e3a
lesterjefframos <35057921+lesterjefframos@users.noreply.github.com>
2022-09-09 14:51:40 +0800

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -6,7 +6,7 @@ const burger = document.querySelector(".burger");
         navMenu.classList.toggle("active");
     })
 
-    document.querySelectorAll(".nav-item").forEach(n => n.addEventListener("click", () => {
-        burger.classList.remove("active");
-        navMenu.classList.remove("active");
-    }))
+    // document.querySelectorAll(".nav-item").forEach(n => n.addEventListener("click", () => {
+    //     burger.classList.remove("active");
+    //     navMenu.classList.remove("active");
+    // }))
